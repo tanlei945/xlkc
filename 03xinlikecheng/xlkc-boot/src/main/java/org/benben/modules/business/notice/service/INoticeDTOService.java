0fd1bf00618f0b0d702cd89d7418b435920d9f6f
jinxin <jinxin@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-05-18 01:59:06 +0000

    用户通知curd

---
+++ 03xinlikecheng/xlkc-boot/src/main/java/org/benben/modules/business/notice/service/INoticeDTOService.java
@@ -0,0 +1,14 @@
+package org.benben.modules.business.notice.service;
+
+import org.benben.modules.business.notice.entity.NoticeDTO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 用户通知
+ * @author： jeecg-boot
+ * @date：   2019-05-18
+ * @version： V1.0
+ */
+public interface INoticeDTOService extends IService<NoticeDTO> {
+
+}
