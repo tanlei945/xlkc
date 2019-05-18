c3df16fed02052ef46cc0b06641333c4474f47bc
jinxin <jinxin@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-05-18 06:45:10 +0000

    通知模块curd

---
+++ 03xinlikecheng/xlkc-boot/src/main/java/org/benben/modules/business/notice/mapper/NoticeDTOMapper.java
@@ -0,0 +1,17 @@
+package org.benben.modules.business.notice.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.benben.modules.business.notice.entity.NoticeDTO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 通知模块
+ * @author： jeecg-boot
+ * @date：   2019-05-18
+ * @version： V1.0
+ */
+public interface NoticeDTOMapper extends BaseMapper<NoticeDTO> {
+
+}
